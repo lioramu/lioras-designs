c1cd29eb6bcf83e5b3e78e626c1baa515af67600
liora muhailov <lioraam87@gmail.com>
2022-07-31 15:50:38 +0300

    hash

--- pages/api/user.js
+++ pages/api/user.js
@@ -9,7 +9,7 @@ const handler = async (req, res) => {
     if (name && email && password) {
       try {
         // Hash password to store it in DB
-        const passwordhash = await bcrypt.sign(password);
+        const passwordhash = await bcrypt.hash(password);
         const user = new User({
           name,
           email,
